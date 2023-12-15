4047f6b11bf1d2e931bc6d4792f1804c38927720
SIDDARTH-P-P <sidhusidharth2711@gmail.com>
2023-12-15 14:49:53 +0530

    almost completed

--- route_handler.js
+++ route_handler.js
@@ -3,28 +3,27 @@ import user from "./schema/user.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt"
 import path from "path"
-import fs from "fs";
-import { log } from "util";
+import fs from "fs"
 
 
-const {sign} = jwt;
+const { sign } = jwt;
 
 //Add user data
-export async function userdata(req,res){
+export async function userdata(req, res) {
     try {
-        let {username,email,phone,password} = req.body
-        let check = await user.findOne({username:username})
-        if(check){
+        let { username, email, phone, password } = req.body
+        let check = await user.findOne({ username: username })
+        if (check) {
             return res.json("Username Already Exists Use other name ! ")
         }
-        let hashed = await bcrypt.hash(password,10)
+        let hashed = await bcrypt.hash(password, 10)
         let result = await user.create({
             username,
             email,
             phone,
-            password:hashed
+            password: hashed
         })
-        if(result){
+        if (result) {
             return res.json("Account Successfully Created");
         }
         return res.json("Error to create account !")
@@ -34,18 +33,18 @@ export async function userdata(req,res){
 }
 
 //Login user
-export async function login(req,res){
+export async function login(req, res) {
     try {
-        let {username,password} = req.body
-        let userdata = await user.findOne({username:username})
-        if(!userdata){
+        let { username, password } = req.body
+        let userdata = await user.findOne({ username: username })
+        if (!userdata) {
             return res.status(404).json("User Not Found !")
         }
-        if(userdata){
-            let result = await bcrypt.compare(password,userdata.password)
-            if(result){
-                let token = await sign({username:userdata.username,id:userdata._id},process.env.SCRET_KEY,{expiresIn:"1h"})
-               return res.json({msg:"Successfully login",token})
+        if (userdata) {
+            let result = await bcrypt.compare(password, userdata.password)
+            if (result) {
+                let token = await sign({ username: userdata.username, id: userdata._id }, process.env.SCRET_KEY, { expiresIn: "1h" })
+                return res.json({ msg: "Successfully login", token })
             }
             res.status(401).json("Invalid username or password")
         }
@@ -57,12 +56,12 @@ export async function login(req,res){
 
 
 //Add movies
-export async function add_item(req,res){
+export async function add_item(req, res) {
     try {
-        let {name,language,year,rating,vote,category,Duration} = req.body
+        let { name, language, year, rating, vote, category, Duration } = req.body
         let images = req.files
-        let check = await schema.findOne({name:name})
-        if(check){
+        let check = await schema.findOne({ name: name })
+        if (check) {
             return res.json("Film Already Exists !")
         }
         let result = await schema.create({
@@ -75,29 +74,29 @@ export async function add_item(req,res){
             Duration,
             images,
         })
-        if(result){
+        if (result) {
             return res.json("Film Successfully addded")
         }
-     return res.json("Error add movie !")
+        return res.json("Error add movie !")
     } catch (error) {
         console.log(error);
     }
 }
 
 //Delete movies
-export async function del_item(req,res){
+export async function del_item(req, res) {
     try {
-        let {id} = req.params;
-        let result = await schema.findOne({_id:id})
-        if(result){
-            result.images.map((item)=>{
+        let { id } = req.params;
+        let result = await schema.findOne({ _id: id })
+        if (result) {
+            result.images.map((item) => {
                 console.log(item.filename);
-                fs.rm(`./images/${item.filename}`,(error)=>{
+                fs.rm(`./images/${item.filename}`, (error) => {
                     console.log(error);
                 })
             })
-            let deletedata = await schema.deleteOne({_id:id})
-            if(deletedata){
+            let deletedata = await schema.deleteOne({ _id: id })
+            if (deletedata) {
                 return res.json("Successfully deleted")
             }
         }
@@ -110,41 +109,41 @@ export async function del_item(req,res){
 
 
 
-export async function get_data(req,res){
+export async function get_data(req, res) {
     let result = await schema.find()
     res.json(result)
 }
 
-export function get_file(req,res){
-    let {file} = req.params
+export function get_file(req, res) {
+    let { file } = req.params
     return res.sendFile(path.resolve(`./images/${file}`))
 }
 
-export async function get_movie(req,res){
-    let {id} = req.params
-    let result = await schema.findOne({_id:id})
+export async function get_movie(req, res) {
+    let { id } = req.params
+    let result = await schema.findOne({ _id: id })
     res.json(result)
 
 }
 
 
-export async function get_edit_data(req,res){
-    let {id} = req.params;
-    let result = await schema.findOne({_id:id})
+export async function get_edit_data(req, res) {
+    let { id } = req.params;
+    let result = await schema.findOne({ _id: id })
     res.json(result);
 }
 
-export async function edit_data(req,res){
-    let {id} = req.params;
-    let {name,language,year,rating,vote,category,Duration} = req.body
-    let update = await schema.updateOne({_id:id},{$set:{name,language,year,rating,vote,category,Duration}})
-    if(update){
-       return res.json("Successfully Updated")
+export async function edit_data(req, res) {
+    let { id } = req.params;
+    let { name, language, year, rating, vote, category, Duration } = req.body
+    let update = await schema.updateOne({ _id: id }, { $set: { name, language, year, rating, vote, category, Duration } })
+    if (update) {
+        return res.json("Successfully Updated")
     }
     res.status(400).json("Error to Update");
 }
 
 
-export function auth(req,res){
+export function auth(req, res) {
     res.status(200).json("login")
 }
--- template/min.js
+++ template/min.js
@@ -1,10 +1,3 @@
-function register(){
-    location.href = `http://localhost:3000/addmovie.html`
-}
-
-function login(){
-    location.href = `http://localhost:3000/register_login.html`
-}
 
 function logout(){
    let out =  confirm("are you sure do you want to logout !");
@@ -16,20 +9,24 @@ function logout(){
 }
 
 
+let movies;
 function print(){
     fetch(`http://localhost:3000/api`)
     .then(res => res.json())
     .then(data => {
         document.getElementById("cart").innerHTML = null;
+        movies = data;
         data.map((item)=>{
+            let category = item.category.split(",").join("/");
             let cart = `
             <div class="col-lg-3 pt-5">
             <a href="http://localhost:3000/details.html?${item._id}">
-            <div class="card" style="width: 18rem;">
+            <div class="card rounded-3" style="width: 18rem;">
                 <img src="http://localhost:3000/get_file/${item.images[0].filename}" class="card-img-top img" alt="...">
+                <h5 class=" rounded-top bi bi-star-fill text-danger ps-4 p  crd"> <span class="text-light ps-1">${item.rating}</span> <span class="text-light ps-3">${item.vote} Votes</span> </h5>
                 <div class="card-body">
                   <h5 class="card-title">${item.name}</h5>
-                  <p class="card-text">${item.category}</p>
+                  <p class="card-text">${category}</p>
                 </div>
               </div>
               </a>
@@ -46,11 +43,36 @@ function print(){
     })
     .then(res =>{
         if(res.status == 200){
-          return  document.getElementById("nav_button").innerHTML = ` <button class="btn btn-warning me-5" onclick="register()">Add Movie</button>  <button onclick="logout()" class="btn btn-danger me-5">Logout</button>`
+          return  document.getElementById("nav_button").innerHTML = `<a href="http://localhost:3000/addmovie.html"><button class="btn btn-warning me-5">Add Movie</button></a>  <button onclick="logout()" class="btn btn-danger me-5">Logout</button>`
         }
         res.json()
         alert("Unauthorized access plese try to login")
     })
-
 }
+
+function search(){
+   let value = document.getElementById("search").value
+   let filter_datalist = movies.filter(item => item.name.toLowerCase().includes(value.toLowerCase()));
+   let list = document.querySelector("#list");
+   list.innerHTML = "";
+   filter_datalist.forEach(item => {
+    let option = `<option value="${item.name}">${item.name}</option>`;
+    list.innerHTML += option;
+   })
+   console.log(filter_datalist);
+}
+
+function search_item(){
+    let value = document.getElementById("search").value
+   let  datas = movies.filter((item)=>item.name == value);
+    console.log(datas);
+    if(datas.length == 0){
+        return alert("Movie not Found !...");
+    }
+    datas.map((item)=>{
+        console.log(item._id);
+        location.href = `http://localhost:3000/details.html?${item._id}`
+    })
+}
+
 print();
